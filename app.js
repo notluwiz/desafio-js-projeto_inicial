7a45963f71f5069ebdd40b9081017258e6b2b222
luwiz <le9851928@gmail.com>
2025-02-04 09:43:15 -0300

    update

---
+++ app.js
@@ -0,0 +1,12 @@
+alert("Boas vindas ao nosso site!");
+
+let nome = "lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemDeErro = "Erro! Preencha todos os campos!";
+
+alert(mensagemDeErro);
+
+nome = prompt ("Digite o nome:");
+idade = prompt ("Digite sua idade:");
